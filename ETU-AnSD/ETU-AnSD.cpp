c9a5dc6d5536512f629d415df7828c8a7582ad59
BoysNeverCry <deadbutnotcry@gmail.com>
2024-10-10 20:20:25 +0300

    Make fucking professor requirements =)

--- ETU-AnSD/ETU-AnSD.cpp
+++ ETU-AnSD/ETU-AnSD.cpp
@@ -4,13 +4,26 @@
 #include <iostream>
 #include "LabOne.h"
 int main() {
-    PolynomialMultiplication pm;
     int num1 = 132;
     int num2 = 257;
+    Polynomial poly1(num1);
+    Polynomial poly2(num2);
 
-    int result = pm.multiply(num1, num2);
+    Polynomial sum = poly1 + poly2;
+    Polynomial diff = poly1 - poly2;
+    Polynomial prod = poly1 * poly2;
 
-    std::cout << "Result of multiply  " << result << std::endl;
+    std::cout << "poly1: " << poly1 << std::endl;
+    std::cout << "poly2: " << poly2 << std::endl;
+    std::cout << "sum: " << sum << std::endl;
+    std::cout << "diff: " << diff << std::endl;
+    std::cout << "prod: " << prod << std::endl;
+
+    std::cout << "sum as number: " << sum.toNumber() << std::endl;
+    std::cout << "diff as number: " << diff.toNumber() << std::endl;
+    std::cout << "prod as number: " << prod.toNumber() << std::endl;
+
+    return 0;
 
     return 0;
 }
--- ETU-AnSD/LabOne.h
+++ ETU-AnSD/LabOne.h
@@ -1,82 +1,155 @@
-﻿#pragma once
-
-class PolynomialMultiplication {
+﻿class Polynomial {
 public:
-	int multiply(int num1, int num2) {
-		// Преобразуем числа в многочлены
-		int* poly1 = numberToPolynomial(num1);
-		int* poly2 = numberToPolynomial(num2);
+    // Конструктор по умолчанию
+    Polynomial() : coefficients(nullptr), size(0) {}
 
-		int size1 = getPolynomialSize(num1);
-		int size2 = getPolynomialSize(num2);
+    // Конструктор с инициализацией коэффициентов
+    Polynomial(int* coeffs, int sz) : size(sz) {
+        coefficients = new int[size];
+        std::memcpy(coefficients, coeffs, size * sizeof(int));
+    }
 
-		int* resultPoly = multiplyPolynomials(poly1, size1, poly2, size2);
+    // Конструктор из числа
+    Polynomial(int num) {
+        size = getPolynomialSize(num);
+        coefficients = new int[size];
+        for (int i = 0; i < size; ++i) {
+            coefficients[i] = num % 10;
+            num /= 10;
+        }
+    }
 
-		int result = polynomialToNumber(resultPoly, size1 + size2 - 1);
+    // Деструктор
+    ~Polynomial() {
+        delete[] coefficients;
+    }
 
-		delete[] poly1;
-		delete[] poly2;
-		delete[] resultPoly;
+    // Копирующий конструктор
+    Polynomial(const Polynomial& other) : size(other.size) {
+        coefficients = new int[size];
+        std::memcpy(coefficients, other.coefficients, size * sizeof(int));
+    }
 
-		return result;
-	}
+    // Оператор присваивания
+    Polynomial& operator=(const Polynomial& other) {
+        if (this != &other) {
+            delete[] coefficients;
+            size = other.size;
+            coefficients = new int[size];
+            std::memcpy(coefficients, other.coefficients, size * sizeof(int));
+        }
+        return *this;
+    }
+
+    // Перегрузка оператора сложения
+    Polynomial operator+(const Polynomial& other) const {
+        int maxSize = std::max(size, other.size);
+        int* resultCoeffs = new int[maxSize];
+        std::memset(resultCoeffs, 0, maxSize * sizeof(int));
+
+        for (int i = 0; i < size; ++i) {
+            resultCoeffs[i] += coefficients[i];
+        }
+
+        for (int i = 0; i < other.size; ++i) {
+            resultCoeffs[i] += other.coefficients[i];
+        }
+
+        // Обработка переносов
+        for (int i = 0; i < maxSize - 1; ++i) {
+            if (resultCoeffs[i] >= 10) {
+                resultCoeffs[i + 1] += resultCoeffs[i] / 10;
+                resultCoeffs[i] %= 10;
+            }
+        }
+
+        return Polynomial(resultCoeffs, maxSize);
+    }
+
+    // Перегрузка оператора вычитания
+    Polynomial operator-(const Polynomial& other) const {
+        int maxSize = std::max(size, other.size);
+        int* resultCoeffs = new int[maxSize];
+        std::memset(resultCoeffs, 0, maxSize * sizeof(int));
+
+        for (int i = 0; i < size; ++i) {
+            resultCoeffs[i] += coefficients[i];
+        }
+
+        for (int i = 0; i < other.size; ++i) {
+            resultCoeffs[i] -= other.coefficients[i];
+        }
+
+        // Обработка заимствований
+        for (int i = 0; i < maxSize - 1; ++i) {
+            if (resultCoeffs[i] < 0) {
+                resultCoeffs[i + 1] -= 1;
+                resultCoeffs[i] += 10;
+            }
+        }
+
+        // Удаление ведущих нулей
+        while (maxSize > 1 && resultCoeffs[maxSize - 1] == 0) {
+            --maxSize;
+        }
+
+        return Polynomial(resultCoeffs, maxSize);
+    }
+
+    // Перегрузка оператора умножения
+    Polynomial operator*(const Polynomial& other) const {
+        int resultSize = size + other.size - 1;
+        int* resultCoeffs = new int[resultSize];
+        std::memset(resultCoeffs, 0, resultSize * sizeof(int));
+
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < other.size; ++j) {
+                resultCoeffs[i + j] += coefficients[i] * other.coefficients[j];
+            }
+        }
+
+        // Обработка переносов
+        for (int i = 0; i < resultSize - 1; ++i) {
+            if (resultCoeffs[i] >= 10) {
+                resultCoeffs[i + 1] += resultCoeffs[i] / 10;
+                resultCoeffs[i] %= 10;
+            }
+        }
+
+        return Polynomial(resultCoeffs, resultSize);
+    }
+
+    // Метод для преобразования многочлена обратно в число
+    int toNumber() const {
+        int result = 0;
+        int power = 1;
+
+        for (int i = 0; i < size; ++i) {
+            result += coefficients[i] * power;
+            power *= 10;
+        }
+
+        return result;
+    }
+
+    // Перегрузка оператора вывода
+    friend std::ostream& operator<<(std::ostream& os, const Polynomial& poly) {
+        for (int i = poly.size - 1; i >= 0; --i) {
+            os << poly.coefficients[i];
+        }
+        return os;
+    }
 
 private:
-	int* numberToPolynomial(int num) {
-		int size = getPolynomialSize(num);
-		int* poly = new int[size];
-		memset(poly, 0, size * sizeof(int));
+    int* coefficients;
+    int size;
 
-		for (int i = 0; i < size; ++i) {
-			poly[i] = num % 10;
-			num /= 10;
-		}
-
-		return poly;
-	}
-
-	int getPolynomialSize(int num) {
-		int size = 0;
-		while (num > 0) {
-			num /= 10;
-			++size;
-		}
-		return size;
-	}
-
-	int* multiplyPolynomials(const int* poly1, int size1, const int* poly2, int size2) {
-		int resultSize = size1 + size2 - 1;
-		int* result = new int[resultSize];
-		memset(result, 0, resultSize * sizeof(int));
-
-		for (int i = 0; i < size1; ++i) {
-			for (int j = 0; j < size2; ++j) {
-				result[i + j] += poly1[i] * poly2[j];
-			}
-		}
-
-		for (int i = 0; i < resultSize - 1; ++i)
-		{
-			if (result[i] >= 10)
-			{
-				result[i + 1] += result[i] / 10;
-				result[i] = result[i] % 10;
-			}
-		}
-
-		return result;
-	}
-
-	// Метод для преобразования многочлена обратно в число
-	int polynomialToNumber(const int* poly, int size) {
-		int result = 0;
-		int power = 1;
-
-		for (int i = 0; i < size; ++i) {
-			result += poly[i] * power;
-			power *= 10;
-		}
-
-		return result;
-	}
+    int getPolynomialSize(int num) const {
+        int size = 0;
+        while (num > 0) {
+            num /= 10;
+            ++size;
+        }
+        return size;
+    }
 };
